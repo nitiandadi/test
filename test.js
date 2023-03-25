e496c4b9558770103775ed49a03a38af7aea80b0
Emperor Nitian <2178508639@qq.com>
2023-03-25 14:09:10 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    sayHello(){
+        console.log(`Hello, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+}
